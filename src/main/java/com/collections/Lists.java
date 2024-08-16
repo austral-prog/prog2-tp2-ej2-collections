06e51b6daad0f6dbeefc06bd88f275ee03a8e788
Matias Ponce <matias.ponce@mulesoft.com>
2024-08-15 23:38:23 -0300

    Oops

--- src/main/java/com/collections/Lists.java
+++ src/main/java/com/collections/Lists.java
@@ -4,49 +4,5 @@ import java.util.List;
 
 public class Lists {
 
-    public static int indexOf(String target, List<String> list) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(target)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public static int indexOfByIndex(String target, List<String> list, int startIndex) {
-        for (int i = startIndex; i < list.size(); i++) {
-            if (list.get(i).equals(target)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public static int indexOfEmpty(List<String> list) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).isEmpty()) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
-    public static int put(String target, List<String> list) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).isEmpty()) {
-                list.set(i, target);
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public static int remove(String target, List<String> list) {
-        int count = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(target)) {
-                list.set(i, "");
-                count++;
-            }
-        }
-        return count;
-    }
 }
